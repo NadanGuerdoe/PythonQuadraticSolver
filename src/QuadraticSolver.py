cde1587df5ad17c8d35f2a92c07c21e078ecaaba
NadanGuerdoe <156608820+NadanGuerdoe@users.noreply.github.com>
2024-10-01 11:13:08 -0400

    Fixed printing

--- src/QuadraticSolver.py
+++ src/QuadraticSolver.py
@@ -5,9 +5,9 @@ Quadratic.py- solves ax**2+bx+c and returns the results.
 
 import numpy as np
 
-a = 2
-b = 4
-c = 1
+a = 23
+b = 233
+c = 23
 
 discriminant = b**2-4*a*c
 vertex = (-b/(2*a))
@@ -15,10 +15,12 @@ vertex = (-b/(2*a))
 if discriminant < 0:
     xpos = (np.sqrt(-discriminant))/(2*a)
     xneg = (np.sqrt(-discriminant))/(2*a)
-    print(vertex,"+", xpos, "i\n",vertex,"-", xneg, "i" )
+    print(vertex,"+", xpos, "i")
+    print(vertex,"-", xneg, "i" )
 elif discriminant > 0:
     xpos = (np.sqrt(discriminant))/(2*a)
     xneg = (-np.sqrt(discriminant))/(2*a)
-    print(vertex + xpos,"\n", vertex + xneg)
+    print(vertex + xpos)
+    print(vertex + xneg)
 else:
     print(vertex)
